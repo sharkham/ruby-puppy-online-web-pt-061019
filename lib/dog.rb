ff97d1d499e49f614b2ab97e223a6668ef818259
Sam Markham <sam.e.markham@gmail.com>
2019-06-27 19:16:10 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+require "pry"
+
+class Dog
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear 
+  end
+end
+
+# fido = Dog.new("Fido")
+# lassie = Dog.new("Lassie")
+# blaze = Dog.new("Blaze")
+#
+# Dog.all
